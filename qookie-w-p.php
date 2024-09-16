5a473b1c28ab6ce9f1491ccdce6a46b87d442b20
Robin Linderoth <robin@doldis.se>
2024-09-16 18:40:31 +0200

    Initial commit

---
+++ qookie-w-p.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: QookieQloud Wordpress Plugin
+Plugin URI: https://qookie.qodli.cloud
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Qod:li AB
+Author URI: https://qodli.se
+License: A "Slug" license name e.g. GPL2
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Enqueue the consentManager.js script and styles
+ */
+function ccm_enqueue_scripts() {
+    // Enqueue the consent manager script
+    wp_enqueue_script('consent-manager', 'https://qookie.qodli.cloud/js/consentLoader.js', array(), '1.0', true);
+
+}
+add_action('wp_enqueue_scripts', 'ccm_enqueue_scripts');
