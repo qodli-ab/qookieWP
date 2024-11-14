1e07d97ab1b0d358defdee0b8b51613525ed75c5
Robin Linderoth <robin@doldis.se>
2024-11-14 17:50:49 +0100

    Updated version

--- qookieqloud.php
+++ qookieqloud.php
@@ -2,12 +2,12 @@
 
 /*
 Plugin Name: QookieQloud™ Consent Management
-Plugin URI: https://qookieqloud.com
+Plugin URI: https://qookieqloud.com/wordpress
 Description: Connects to and integrates Cookie-Consent-Manager from QookieQloud™ by Qodli AB.
-Version: 1.2.10
+Version: 1.2.1
 Author: Qod:li AB
 Author URI: https://qodli.se
-License: GPL2
+License: GPLv2
 */
 
 if (!defined('ABSPATH')) exit;
